56a8bcd259ddebf93c22888f3b7d1f7732e54430
Enes Sahin <enessahinn34@gmail.com>
2024-10-29 14:22:22 +0300

    Admin Panel Done.

--- myPortfolio/DAL/Entities/Message.cs
+++ myPortfolio/DAL/Entities/Message.cs
@@ -3,11 +3,10 @@
     public class Message
     {
         public int MessageId { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
+		public string NameSurname { get; set; }
+		public string Subject { get; set; }
         public string Email { get; set; }
         public string MessageDetail { get; set; }
-
         public DateTime SendDate { get; set; }
         public bool IsRead { get; set; }
 
